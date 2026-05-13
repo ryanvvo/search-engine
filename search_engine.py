54720b00f6c5f70c3fa1188497ae18a38267a060
ryanv <ryan.v.vo@gmail.com>
2026-05-12 21:49:45 -0700

    Added README and compressed logic into functions

--- search_engine.py
+++ search_engine.py
@@ -9,27 +9,46 @@ import warnings
 warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning)
 
 
+def tokenize(text):
+    '''
+    Reads in text file and returns a list of the tokens in that file. For the purposes of this project,
+    a token is a sequence of alphanumeric characters, independent of capitalization (so Apple, apple, aPpLe are the same token).
+    returns List<Token>
+    '''
+
+    tokens = re.findall(r"[a-z0-9]+", text.lower())
+    total = len(tokens)
+    counts = Counter(token for token in tokens)
+
+    return counts, total
+
+def open_file(path):
+    '''
+    Opens the file on path and counts the tokens, returning the url and token count.
+    '''
+    with zf.open(path, 'r') as f:
+        data = json.load(f)
+    url = data['url']
+    content = data['content']
+
+    soup = BeautifulSoup(content, 'html.parser')
+    text = soup.get_text(strip=True)
+    count, total = tokenize(text)
+    return url, count, total
 
 number_indexed= 0
 
 r_index = defaultdict(list)
 count = 0
-with zipfile.ZipFile("analyst.zip", "r") as zf:        #currently just looking at analyst
+
+with zipfile.ZipFile("developer.zip", "r") as zf:
     for filename in zf.namelist():
         if filename.endswith(".json"):
-            with zf.open(filename) as f:
-                data = json.load(f)
-
-            html = data['content']
-            soup = BeautifulSoup(html, "html.parser")
-            
-            text = soup.get_text(separator=" ", strip=True)
-            words = re.findall(r"\b\w+\b", text.lower())
-            word_count = Counter(words)
-            #print(word_count)
+            url, word_count, _ = open_file(filename)
+            # print(url, len(word_count)) # debug
 
             for key in word_count.keys():
-                r_index[key].append([filename, word_count[key]]) #name for now, can also easily change it to be the file number. format: [file_name, count_of_word]
+                r_index[key].append((filename, word_count[key])) #name for now, can also easily change it to be the file number. format: [file_name, count_of_word]
 
             number_indexed += 1
 
