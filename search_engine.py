36182ec962f622052f57bc3b11a57d95a0f963e2
ryanv <ryan.v.vo@gmail.com>
2026-05-15 00:43:13 -0700

    Added partial indices, main function

--- search_engine.py
+++ search_engine.py
@@ -1,10 +1,10 @@
+import sys
 import zipfile
 import json
 import re
 import os
 import heapq
 from bs4 import BeautifulSoup, XMLParsedAsHTMLWarning, MarkupResemblesLocatorWarning
-from pathlib import Path
 from collections import Counter, defaultdict
 import warnings
 from nltk.stem import PorterStemmer
@@ -17,7 +17,7 @@ warnings.filterwarnings("ignore", category=MarkupResemblesLocatorWarning)
 PATH = 'developer.zip' # switch to analyst for debugging
 TAG_WHITELIST = ['title','h1','h2','h3', 'h4', 'h5', 'h6','b','strong']
 WEIGHTS = {'title': 4,'h1': 2, 'h2': 2, 'h3': 2,'h4': 1, 'h5': 1, 'h6': 1, 'b': 1, 'strong': 1 }
-
+MAX_INDEX_SIZE = 10 * 1024 * 1024 # 10 mb
 def tokenize(text):
     '''
     Reads in text file and returns a normalized list.
@@ -27,7 +27,7 @@ def tokenize(text):
 
     return (match.group() for match in re.finditer(r"[a-z0-9]+", text.lower()))
 
-def open_file(path):
+def open_file(zf, path):
     '''
     Opens the file on path and counts the tokens, returning the url and token count.
     '''
@@ -58,60 +58,129 @@ def open_file(path):
         total += 1
     return url, count, total
 
-stemmer = PorterStemmer()
+def dump_json(data, dest, partial=False):
+    """
+    Dumps the dictionary into the destination file.
+    """
+    print(f"Dumping {dest}...")
+    dumping_pre_t = time.perf_counter()
+    with open(dest, "w") as f:
+        if partial:
+            for token in sorted(data.keys()):
+                f.write(json.dumps({token: data[token]}) + "\n")
+        else:
+            json.dump(data, f)
+    print(f"Dumping finished. {time.perf_counter() - dumping_pre_t:.4f} seconds.")
 
-page_id = 0
-r_index = defaultdict(list) # swapped to max-heap for more efficient retrieval of top k results, format: [stemmed token: list[(-count, doc id)]]
-id_mapping = {}
-with zipfile.ZipFile(PATH, "r") as zf:
-    for filename in zf.namelist():
-        if filename.endswith(".json"):
-            if __debug__: debug_pre_t = time.perf_counter()
-            url, word_count, total = open_file(filename)
-            if __debug__: print(filename, len(word_count), total, f"{time.perf_counter()-debug_pre_t:.2f}")
-            for key in word_count.keys():
-                heapq.heappush(r_index[stemmer.stem(key)], (-word_count[key], page_id)) # format: [doc id: list[postings, count]]
-            id_mapping[page_id] = url
-            page_id += 1
-        elif __debug__:
-            print(filename)
+def iter_index(path):
+    """ Yields from a partial index to avoid loading entire partial index."""
+    with open(path, 'r') as f:
+        for line in f:
+            d = json.loads(line)
+            token = next(iter(d))
+            yield token, d[token]
 
-print("Dumping index.json...")
-dumping_pre_t = time.perf_counter()
-with open("index.json", "w") as f:
-    json.dump(r_index, f)
-print(f"Dumping finished. {time.perf_counter() - dumping_pre_t:.4f} seconds.")
-print("Dumping mapping.json...")
-dumping_pre_t = time.perf_counter()
-with open("mapping.json", "w") as f:
-    json.dump(id_mapping, f)
-print(f"Dumping finished. {time.perf_counter() - dumping_pre_t:.4f} seconds.")
+def merge_indices(num_of_indices, dest='index.json'):
+    """ Merges the partial indices to a single index. """
+    print("Merging partial indices...")
+    start = time.perf_counter()
+    iterators = [iter_index(f"index{i}.json") for i in range(num_of_indices)]
+    heap = []
 
+    for i, it in enumerate(iterators):
+        try:
+            token, postings = next(it)
+            heapq.heappush(heap, (token, i, postings))
+        except StopIteration:
+            pass
 
-total_size = os.path.getsize("index.json") / 1024 #get in bytes, then convert to KB
-end_time = time.perf_counter()
+    with open(dest, 'w') as f:
+        while heap:
+            current_token, i, postings = heapq.heappop(heap)
+            merged = list(postings)
 
-delta_t = end_time - start_time
-delta_min = (end_time - start_time) // 60
-delta_sec = (end_time - start_time) % 60
+            while heap and heap[0][0] == current_token: # look at other iterators for same token
+                _, j, more = heapq.heappop(heap)
+                merged.extend(more)
+                try:
+                    token, p = next(iterators[j])
+                    heapq.heappush(heap, (token, j, p))
+                except StopIteration:
+                    pass
 
-print(f"Execution time: {end_time - start_time:.4f} seconds\n")
-print("Number of indexed documents:", len(id_mapping))
-print("Number of unique tokens after stemming:", len(r_index))
-print("Size in KB:", f"{total_size:.2f}")
+            merged.sort(key=lambda x: x[0], reverse=True)
+            f.write(json.dumps({current_token: merged}) + '\n')
 
-def update_stats():
-    '''
-    Writes to output.txt the analytics.
-    '''
+            try:
+                token, p = next(iterators[i])
+                heapq.heappush(heap, (token, i, p))
+            except StopIteration:
+                pass
+    print(f"Merging finished. {time.perf_counter() - start:.4f} seconds.")
+
+def main():
+    stemmer = PorterStemmer()
+
+    page_id = 0
+    indices = 0
+    unique_tokens = set() # REMOVE AFTER M1
+    r_index = defaultdict(list) # swapped to max-heap for more efficient retrieval of top k results, format: [stemmed token: list[(-count, doc id)]]
+    id_mapping = {}
+    with zipfile.ZipFile(PATH, "r") as zf:
+        for filename in zf.namelist():
+            if filename.endswith(".json"):
+                if __debug__: debug_pre_t = time.perf_counter()
+                url, word_count, total = open_file(zf, filename)
+                if __debug__: print(filename, len(word_count), total, f"{time.perf_counter()-debug_pre_t:.2f}")
+                for key in word_count.keys():
+                    r_index[stemmer.stem(key)].append((page_id, word_count[key])) # format: [doc id: list[postings, count]]
+                    unique_tokens.add(stemmer.stem(key))
+                id_mapping[page_id] = url
+                page_id += 1
+
+                if sys.getsizeof(r_index) > MAX_INDEX_SIZE:
+                    print("Max index size reached, dumping to disk...")
+                    dump_json(r_index, f"index{indices}.json", partial=True)
+                    r_index.clear()
+                    indices += 1
+
+            elif __debug__:
+                print(filename)
+
+    dump_json(r_index, f"index{indices}.json", partial=True)
+
+    merge_indices(indices+1)
+
+    dump_json(id_mapping, "id_mapping.json")
+
+    total_size = os.path.getsize("index.json") / 1024 #get in bytes, then convert to KB
+    end_time = time.perf_counter()
+
+    delta_min = (end_time - start_time) // 60
+    delta_sec = (end_time - start_time) % 60
+    print("Number of partial indices made:", indices+1)
+    print("Removing partial indices...")
+    for p in [f"index{i}.json" for i in range(indices+1)]:
+        os.remove(p)
+
+    print(f"Execution time: {end_time - start_time:.4f} seconds\n")
+    print("Number of indexed documents:", len(id_mapping))
+    print("Number of unique tokens after stemming:", len(unique_tokens))
+    print("Size in KB:", f"{total_size:.2f}")
+
+    # Output.txt
     temp = "output.txt.tmp"
     fin = "output.txt"
 
     with open(temp, "w") as outFile:
-        outFile.write(f"Execution time: {delta_min} minutes, {delta_sec:.2f} seconds\n\n")
+        outFile.write(f"Execution time: {delta_min} minutes, {delta_sec:.2f} seconds\n")
+        outFile.write(f"Number of partial indices made: {indices + 1}\n\n")
+
         outFile.write(f"Number of indexed documents: {len(id_mapping)}\n")
-        outFile.write(f"Number of unique tokens after stemming: {len(r_index)}\n")
+        outFile.write(f"Number of unique tokens after stemming: {len(unique_tokens)}\n")
         outFile.write(f"Size in KB: {total_size:.2f}\n\n")
 
     os.replace(temp, fin)
-update_stats()
+
+if __name__ == '__main__':
+    main()
